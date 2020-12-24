cbc8efd4e98060f45f810bec45088068c20a8e1f
dogezhou <doge.zhou@outlook.com>
2020-12-24 16:55:37 +0800

    chore: add sourcemap

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,5 @@
 import typescript from "rollup-plugin-typescript2";
+import sourceMaps from 'rollup-plugin-sourcemaps'
 import { terser } from 'rollup-plugin-terser'
 
 const libraryName = 'foo-lib'
@@ -6,13 +7,14 @@ const libraryName = 'foo-lib'
 export default {
     input: "./src/main.ts",
     output: [
-        { format: "es", file: 'dist/main.esm.js' },
-        { format: "umd", file: 'dist/main.umd.js', name: libraryName },
+        { format: "es", file: 'dist/main.esm.js', sourcemap: true },
+        { format: "umd", file: 'dist/main.umd.js', name: libraryName, sourcemap: true },
     ],
     plugins: [
         typescript({
             useTsconfigDeclarationDir: true
         }),
-        terser()
+        sourceMaps(),
+        terser(),
     ]
 }
